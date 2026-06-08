81e5c071b7557944a85623ac7cbccffc715b3a93
sachijhaa <sachi.st.dps@gmail.com>
2026-06-08 20:21:08 +0530

    Updates server.js for deployment final

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173",
+           "https://mini-social-app-new.vercel.app"],
+    credentials:true
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
